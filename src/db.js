551895526e3e958dca6b020b47c1d79826eb8bbe
r-gabriel-f <r.gabriel.f@outlook.com>
2024-09-10 21:44:26 -0400

    version1

--- src/db.js
+++ src/db.js
@@ -4,6 +4,17 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
-    }
+    },
+    max: 20, // máximo número de clientes en el pool
+    idleTimeoutMillis: 30000, // tiempo máximo que un cliente puede estar inactivo en el pool
+    connectionTimeoutMillis: 2000, // tiempo máximo para establecer una nueva conexión
 });
-module.exports = pool;
+
+pool.on('error', (err, client) => {
+    console.error('Error inesperado en el cliente inactivo', err);
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+    getClient: () => pool.connect(),
+};
